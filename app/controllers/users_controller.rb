3174a58a075a84e62cd1f3a9361bf68cec01dab2
Cindy <cindy-ta@uiowa.edu>
2016-09-27 21:54:31 +0000

    work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def user_params
-    params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
+    params.require(:user).permit(:user_id, :email, :session_token)
   end
   
   def show
