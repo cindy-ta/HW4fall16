7c735bd97a5e23b81e95d6b6264ab55fec5d08b7
Cindy <cindy-ta@uiowa.edu>
2016-09-27 17:16:18 +0000

    ??

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
+  end
+  
+  def show
+    id = params[:id]
+    @user = User.find(id)
+  end
+  
+  def index
+    @user = User.all
+  end
+  
+  def new
+    # default: render 'new' template
+  end
+    
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.user_id} was successfully created."
+    redirect_to users_path
+  end
+  
+  def edit
+    @user = User.find params[:id]
+  end
+  
+  def update
+    @user = User.find params[:id]
+    @user.update_attributes!(user_params)
+    flash[:notice] = "#{@movie.title} was successfully update."
+    redirect_to user_path(@user)
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "User '#{user.title}' deleted."
+    redirect_to user_path
+  end
+  
+end
